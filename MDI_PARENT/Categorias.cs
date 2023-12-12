f8db88a61f61c0b0d74034296521640fab360a67
Gabriel Ferreira Carvalho <118980722+gabrieloakz@users.noreply.github.com>
2023-12-12 18:18:57 +0000

    ss

---
+++ MDI_PARENT/Categorias.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDI_PARENT
+{
+    public class Categorias
+    {
+        private int codigo;
+        private string categoria;
+        private string zona;
+        private int fila;
+        private int prateleira;
+
+        //construtor vazio
+        public Categorias() 
+        { 
+            this.codigo = 0;
+            this.categoria = null;
+            this.zona = null;
+            this.fila = 0;
+            this.prateleira = 0;
+        }
+
+        //construtor de facto
+        public Categorias(int codigo, string categoria, string zona, int fila, int prateleira) 
+        { 
+            this.codigo = codigo;
+            this.categoria = categoria;
+            this.zona = zona;
+            this.fila = fila;
+            this.prateleira = prateleira;
+        }
+    }
+}
