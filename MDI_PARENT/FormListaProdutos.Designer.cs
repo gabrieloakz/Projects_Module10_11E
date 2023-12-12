9475e1caac2cf674fc46bfa46bbb8930a6919141
Gabriel Ferreira Carvalho <118980722+gabrieloakz@users.noreply.github.com>
2023-12-12 17:38:23 +0000

    final version

--- MDI_PARENT/FormListaProdutos.Designer.cs
+++ MDI_PARENT/FormListaProdutos.Designer.cs
@@ -60,7 +60,7 @@
             this.statusMsg});
             this.statusStrip1.Location = new System.Drawing.Point(0, 428);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(491, 22);
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -74,7 +74,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(491, 450);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.grelha);
             this.Controls.Add(this.buttonSair);
